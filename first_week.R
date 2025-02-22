30b61c696b5a50a3c8809d0e6c7b0fc5ed955a46
bettuncay <116023734+bettuncay@users.noreply.github.com>
2025-02-22 20:28:46 +0300

    Update first_week.R

--- first_week.R
+++ first_week.R
@@ -1,6 +1,6 @@
 install.packages("readr")  # Sadece ilk kez y??klemek i??in
 library(readr)
-#Veri Setini i??e Aktaral??m
+#Veri Setini ice Aktaral??m
 bike_csv <- read.csv("C:/Users/Casper/Desktop/SeoulBikeData.csv")
 #Veri Yap??s??n?? ??nceleyelim
 str(bike_csv)
@@ -10,41 +10,41 @@ colnames(bike_csv)
 total_bikes <-sum(bike_csv$`Rented.Bike.Count`, na.rm = TRUE)
 total_bikes
 
-#Ortalama s??cakl??k:
+#Ortalama sicaklik:
 mean_temp <- mean(bike_csv$`Temperature..C.`,na.rm = TRUE)
 mean_temp
 
-#Maksimum r??zgar h??z??:
+#Maksimum ruzgar hizi:
 max_wind <- max(bike_csv$`Wind.speed..m.s`,na.rm=T)
 max_wind
 
-#Mevsim de??i??kenini fakt??r yapal??m.
+#Mevsim degiskenini faktor yapal??m.
 fseason <-factor(bike_csv$Seasons)
 levels(fseason)
 table(bike_csv$Seasons) #her bir levelin frekanslar??n?? g??sterir
 
-#Saatleri i??eren bir vekt??r olu??tural??m.
+#Saatleri iceren bir vektor olustural??m
 hours <-(bike_csv$Hour)
 hours
 #Eksik Veri var m???
 missing_values <- sum(is.na(bike_csv))
 missing_values
 colSums(is.na(bike_csv))
-#Bir matris olu??tural??m
+#Bir matris olustural??m
 temp_m <- matrix(c(bike_csv$Temperature..C.,bike_csv$Humidity...), ncol=2,byrow=FALSE)
 colnames(temp_m) <- c("temperature","humidity")
 temp_m
-#Olu??turdu??umuz matrise g??z atal??m:
+#Olusturdugumuz matrise goz atal??m:
 head(temp_m)
 dim(temp_m)
 nrow(temp_m)
 ncol(temp_m)
 
-#Liste olu??tural??m
+#Liste olustural??m
 bike_list <- list(data = bike_csv, hours = hours, temp_humidity = temp_m)
 bike_list
 
-#??zetler
+#Ozetler
 summary(bike_csv)
 head(temp_m)
 str(bike_list)
@@ -55,18 +55,18 @@ glimpse(bike_list)
 #Data Frames
 head(bike_csv, 10)
 
-#Ka?? tatil g??n?? oldu??una bakal??m:
+#Kac tatil gunu olduguna bakal??m:
 fholiday <- factor(bike_csv$Holiday)
 levels(fholiday)
 table(fholiday)
 
-#Merging Vectors
+#Merging Vectors -> Vektorleri birlestirelim
 rainfall <- c(bike_csv$Rainfall.mm.)
 rainfall
 snowfall <- c(bike_csv$Snowfall..cm.)
 snowfall
 weather_conditions <- cbind(rainfall,snowfall)
-#Basit bir Grafik ??izelim (Ekstra)
+#Basit bir grafik cizelim (Ekstra)
 library(ggplot2)
 ggplot(bike_csv, aes(x = `Rented.Bike.Count`)) +
   geom_histogram(binwidth = 100, fill = "pink", color = "green") +
